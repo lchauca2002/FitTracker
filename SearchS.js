ed23a74b1e1d2267ceb62730f6070b277cc41eca
lchauca2002 <112422569+lchauca2002@users.noreply.github.com>
2024-03-20 11:35:53 -0400

    Update SearchS.js

--- SearchS.js
+++ SearchS.js
@@ -38,7 +38,6 @@ for (let exercise of exercises) {
 }
 
 function showFormAndSelectExercise(selectedExercise) {
-
     if (existingForm) {
         const exerciseInput = existingForm.querySelector('input[name="exercisename"]');
         if (exerciseInput) {
@@ -50,59 +49,175 @@ function showFormAndSelectExercise(selectedExercise) {
             newExerciseInput.value = selectedExercise;
             newExerciseInput.readOnly = true;
 
-            existingForm.appendChild(newExerciseInput);
+            const nameLabel = document.createElement('label');
+            nameLabel.textContent = 'Exercisename';
+            nameLabel.classList.add('form-label');
+
+            const divCol = document.createElement('div');
+            divCol.classList.add('col-md-4');
+            divCol.appendChild(nameLabel);
+            divCol.appendChild(newExerciseInput);
+
+            existingForm.appendChild(divCol);
         }
-        return; 
+        return;
     }
 
     // Create a new form
     const form = document.createElement('form');
     form.action = '/exerciselog';
     form.method = 'post';
+    form.setAttribute('novalidate', '');
+    form.classList.add('row', 'g-2', 'needs-validation');
+
+    // Exercisetype
+    const divCol1 = document.createElement('div');
+    divCol1.classList.add('col-md-4');
+    const labelType = document.createElement('label');
+    labelType.setAttribute('for', 'validationCustom01');
+    labelType.textContent = 'Exercisetype';
+    labelType.classList.add('form-label');
 
     const exercisetypeInput = document.createElement('input');
     exercisetypeInput.type = 'text';
+    exercisetypeInput.classList.add('form-control');
+    exercisetypeInput.id = 'validationCustom01';
     exercisetypeInput.name = 'exercisetype';
     exercisetypeInput.value = 'strength';
     exercisetypeInput.readOnly = true;
 
+    divCol1.appendChild(labelType);
+    divCol1.appendChild(exercisetypeInput);
+
+    // Exercisename 
+    const divCol2 = document.createElement('div');
+    divCol2.classList.add('col-md-4');
+    const labelName = document.createElement('label');
+    labelName.setAttribute('for', 'validationCustom02');
+    labelName.textContent = 'Exercisename';
+    labelName.classList.add('form-label');
+
     const exerciseInput = document.createElement('input');
     exerciseInput.type = 'text';
+    exerciseInput.classList.add('form-control');
+    exerciseInput.id = 'validationCustom02';
     exerciseInput.name = 'exercisename';
     exerciseInput.value = selectedExercise.trim();
     exerciseInput.readOnly = true;
+    divCol2.appendChild(labelName);
+    divCol2.appendChild(exerciseInput);
+
+    // Empty column
+    const divEmpty = document.createElement('div');
+    divEmpty.classList.add('col-md-4');
+
+    //NumOfSets
+    const divCol3 = document.createElement('div');
+    divCol3.classList.add('col-md-4');
+    const labelNumOfSets = document.createElement('label');
+    labelNumOfSets.setAttribute('for', 'validationCustom03');
+    labelNumOfSets.textContent = 'NumOfSets';
+    labelNumOfSets.classList.add('form-label');
 
     const numofsetsInput = document.createElement('input');
     numofsetsInput.type = 'number';
+    numofsetsInput.classList.add('form-control');
+    numofsetsInput.id = 'validationCustom03';
     numofsetsInput.name = 'numofsets';
-    numofsetsInput.placeholder = 'Num Of Sets';
+    numofsetsInput.placeholder = 'Number Of Sets';
+    numofsetsInput.setAttribute('required', '');
+    const divfeedback = document.createElement('div');
+    divfeedback.classList.add('invalid-feedback');
+    divfeedback.textContent = 'Please provide valid numofsets.';
+    divCol3.appendChild(labelNumOfSets);
+    divCol3.appendChild(numofsetsInput);
+    divCol3.appendChild(divfeedback);
+
+    //Repetitions
+    const divCol4 = document.createElement('div');
+    divCol4.classList.add('col-md-4');
+    const labelRepetitions = document.createElement('label');
+    labelRepetitions.setAttribute('for', 'validationCustom04');
+    labelRepetitions.textContent = 'Repetitions';
+    labelRepetitions.classList.add('form-label');
 
     const repetitionsInput = document.createElement('input');
     repetitionsInput.type = 'number';
+    repetitionsInput.classList.add('form-control');
+    repetitionsInput.id = 'validationCustom04';
     repetitionsInput.name = 'repetitions';
-    repetitionsInput.placeholder = 'Num Of Repetitions';
+    repetitionsInput.placeholder = 'Number of Repetitions';
+    repetitionsInput.setAttribute('required', '');
+    const divfeedback2 = document.createElement('div');
+    divfeedback2.classList.add('invalid-feedback');
+    divfeedback2.textContent = 'Please provide valid repetitions.';
+    divCol4.appendChild(labelRepetitions);
+    divCol4.appendChild(repetitionsInput);
+    divCol4.appendChild(divfeedback2);
+
+    // Empty column
+    const divEmpty2 = document.createElement('div');
+    divEmpty2.classList.add('col-12');
+
+    //Weight
+    const divCol5 = document.createElement('div');
+    divCol5.classList.add('col-md-4');
+    const labelWeight = document.createElement('label');
+    labelWeight.setAttribute('for', 'validationCustom05');
+    labelWeight.textContent = 'Weight';
+    labelWeight.classList.add('form-label');
 
     const weightInput = document.createElement('input');
     weightInput.type = 'number';
+    weightInput.classList.add('form-control');
+    weightInput.id = 'validationCustom05';
     weightInput.name = 'weight';
-    weightInput.placeholder = 'Weight Amount';
+    weightInput.placeholder = 'Amount of Weight';
+    weightInput.setAttribute('required', '');
+    const divfeeback3 = document.createElement('div');
+    divfeeback3.classList.add('invalid-feedback');
+    divfeeback3.textContent = 'Please provide valid weight.';
+    divCol5.appendChild(labelWeight);
+    divCol5.appendChild(weightInput);
+    divCol5.appendChild(divfeeback3);
 
+    // Submit button
+    const divCol6 = document.createElement('div');
+    divCol6.classList.add('col-12');
     const submitButton = document.createElement('button');
+    submitButton.classList.add('btn', 'btn-primary');
     submitButton.type = 'submit';
-    submitButton.textContent = 'Add Exercise';
-    submitButton.className = 'formbtn';
+    submitButton.textContent = 'Submit form';
+    divCol6.appendChild(submitButton);
 
-    form.appendChild(exercisetypeInput);
-    form.appendChild(exerciseInput);
-    form.appendChild(numofsetsInput);
-    form.appendChild(repetitionsInput);
-    form.appendChild(weightInput);
-    form.appendChild(submitButton);
+    form.appendChild(divCol1);
+    form.appendChild(divCol2);
+    form.appendChild(divEmpty);
+    form.appendChild(divCol3);
+    form.appendChild(divCol4);
+    form.appendChild(divEmpty2);
+    form.appendChild(divCol5);
+    form.appendChild(divCol6);
 
     document.body.appendChild(form);
     existingForm = form;
 
-    form.addEventListener('submit', function () {
+    form.addEventListener('submit', function (event) {
+        // Check if any required fields are empty
+        const reqInput = form.querySelectorAll('input[required]');
+        let emptyF = false;
+        reqInput.forEach(input => {
+            if (!input.value.trim()) {
+                emptyF = true;
+                input.classList.add('is-invalid');
+            } else {
+                input.classList.remove('is-invalid');
+            }
+        });
+        if (emptyF) {
+            event.preventDefault();
+            return false;
+        }
         existingForm = null;
     });
 }

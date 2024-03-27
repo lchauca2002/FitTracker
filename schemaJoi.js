d2cf5eb417d37608a8ea615bc4fbc37d6e7912be
lchauca2002 <112422569+lchauca2002@users.noreply.github.com>
2024-03-27 12:53:14 -0400

    Update schemaJoi.js

--- schemaJoi.js
+++ schemaJoi.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 
 const elSchema = Joi.object({
+    date: Joi.date().required(),
     exercisetype: Joi.string().valid('cardio', 'strength').required(),
     exercisename: Joi.string().required(),
     duration: Joi.when('exercisetype', {
@@ -30,4 +31,38 @@ const elSchema = Joi.object({
     })
 });
 
-module.exports = elSchema;
+
+
+const updateSchema = Joi.object({
+    date: Joi.date().optional(),
+    exercisetype: Joi.string().valid('cardio', 'strength').required(),
+    exercisename: Joi.string().required(),
+    duration: Joi.when('exercisetype', {
+        is: 'cardio',
+        then: Joi.number().required(),
+        otherwise: Joi.number().optional()
+    }),
+    calories: Joi.when('exercisetype', {
+        is: 'cardio',
+        then: Joi.number().required(),
+        otherwise: Joi.number().optional()
+    }),
+    numofsets: Joi.when('exercisetype', {
+        is: 'strength',
+        then: Joi.number().required(),
+        otherwise: Joi.number().optional()
+    }),
+    repetitions: Joi.when('exercisetype', {
+        is: 'strength',
+        then: Joi.number().required(),
+        otherwise: Joi.number().optional()
+    }),
+    weight: Joi.when('exercisetype', {
+        is: 'strength',
+        then: Joi.number().required(),
+        otherwise: Joi.number().optional()
+    })
+});
+
+module.exports = {elSchema, updateSchema };
+
